e77c8211cb6891fde4d50da22f3d2cd15b9425e5
Ha-limLee <leehalim09@gmail.com>
2023-04-23 13:12:15 +0900

    removed unused action

--- src/features/todo/action.js
+++ src/features/todo/action.js
@@ -5,8 +5,5 @@ import { createAction } from "redux-actions";
  * @typedef {import("./api").Todo} Todo
  */
 
-export const GET_TODO = 'Todo/GET_TODO';
-export const getTodo = createAction(GET_TODO);
-
 export const ADD_TODO = 'Todo/ADD_TODO';
 export const addTodo = /** @type {typeof createAction<Todo>} */ (createAction)(ADD_TODO);
