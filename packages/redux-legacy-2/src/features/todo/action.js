772f5ca158a04bb0b096bd500c091e48a0ec31aa
Ha-limLee <leehalim09@gmail.com>
2023-04-24 19:46:51 +0900

    impled union reducer map

--- packages/redux-legacy-2/src/features/todo/action.js
+++ packages/redux-legacy-2/src/features/todo/action.js
@@ -2,8 +2,36 @@
 import { createAction } from "redux-actions";
 
 /**
+ * @template T1, R
+ * @typedef {import("redux-actions").ActionFunction1<T1, R>} ActionFunction1
+ */
+
+/**
  * @typedef {import("./api").Todo} Todo
  */
 
 export const SET_TODO = 'Todo/ADD_TODO';
 export const setTodo = /** @type {typeof createAction<Todo>} */ (createAction)(SET_TODO);
+
+export const REMOVE_TODO = 'Todo/REMOVE_TODO';
+export const removeTodo =
+  /** @type {typeof createAction<Pick<Todo, 'id'>>} */ (createAction)(REMOVE_TODO);
+
+/**
+ * @template P
+ * @typedef {P extends ActionFunction1<infer T, infer R> ? T : never} ExtractPayload1
+ */
+
+/**
+ * @typedef {{
+ *  [SET_TODO]: ExtractPayload1<typeof setTodo>;
+ *  [REMOVE_TODO]: ExtractPayload1<typeof removeTodo>;
+ * }} PayloadMap
+ */
+
+/**
+ * @template T
+ * @typedef {{
+ *  [P in keyof PayloadMap]: (state: T, action: {type: string; payload: PayloadMap[P]}) => T
+ * }} ReducerMap
+ */
--- packages/redux-legacy-2/src/features/todo/reducer.js
+++ packages/redux-legacy-2/src/features/todo/reducer.js
@@ -1,6 +1,6 @@
 // @ts-check
 import { handleActions } from "redux-actions";
-import { SET_TODO } from "./action";
+import { SET_TODO, REMOVE_TODO } from "./action";
 
 /**
  * @typedef {import("./api").Todo} Todo
@@ -10,20 +10,32 @@ import { SET_TODO } from "./action";
  * @typedef {{ [id: Pick<Todo, 'id'>['id']]: Todo }} InitialState
  */
 
+/**
+ * @typedef {import("./action").ReducerMap<InitialState>} ReducerMap
+ */
+
 /** @type {InitialState} */
 const initialState = {};
 
 const handleTodoActions =
-  /** @type {typeof handleActions<typeof initialState, Todo>} */ (handleActions);
+  /** @type {typeof handleActions<typeof initialState, Todo | Pick<Todo, 'id'>>} */ (handleActions);
 
+// TODO: Make function that returns reducer that accepts union payload type
 export default handleTodoActions(
-  {
+  /** @type {ReducerMap} */
+  ({
     [SET_TODO]: (state, { payload }) => {
       return {
         ...state,
         [payload.id]: { ...payload },
       };
     },
-  },
+    [REMOVE_TODO]: (state, { payload }) => {
+      delete state[payload.id];
+      return {
+        ...state,
+      }
+    },
+  }),
   initialState,
 );
