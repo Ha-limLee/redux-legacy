4b6e6537e2f5f213c4e9eeb69d38d79196ef43a3
Ha-limLee <leehalim09@gmail.com>
2023-05-06 21:17:28 +0900

    removed unused codes

--- packages/redux-legacy-3/src/features/todo/reducer.js
+++ packages/redux-legacy-3/src/features/todo/reducer.js
@@ -1,24 +1,6 @@
 // @ts-check
-import { handleAction } from "redux-actions";
+import { handleActions } from "../../app/actions";
 import { SET_TODO, REMOVE_TODO } from "./action";
-import { combineReducers } from "redux";
-
-/**
- * @template S, P
- * @typedef {|
-*  import("redux-actions").ReduxCompatibleReducer<S, P>
-* } ReduxCompatibleReducer
-*/
-
-/**
- * @template Payload
- * @typedef {import("redux-actions").Action<Payload>} Action
- */
-
-/**
- * @template S
- * @typedef {import("./action").TodoReducerMap<S>} TodoReducerMap
- */
 
 /**
  * @typedef {import("./api").Todo} Todo
@@ -26,45 +8,6 @@ import { combineReducers } from "redux";
 
 /** @type {{ [id: Pick<Todo, 'id'>['id']]: Todo }} */
 const initialState = {};
-
-/**
- * @template S
- * @template P
- * @template {{
- *  [K: string]: (state: S, action: {type: string; payload: P}) => S;
- * }} M
- * @param {M} reducerMap
- * @param {S} initialState
- */
-const handleActions = (reducerMap, initialState) => {
-  const actionTypes = Object.keys(reducerMap);
-  const reducers = actionTypes.reduce((acc, actionType) => {
-    const reducer = reducerMap[actionType];
-    acc[actionType] = handleAction(actionType, reducer, initialState);
-    return acc;
-  }, {});
-  const ret = combineReducers(reducers);
-
-  /**
-   * @template T
-   * @typedef {T[keyof T]} ValueOf
-   */
-
-  /**
-   * @template F
-   * @typedef {|
-   *  F extends (state: S, action: {type: string; payload: infer P}) => S ? P : never
-   * } ExtractPayload
-   */
-
-  /**
-   * @typedef {|
-   *  ExtractPayload<ValueOf<typeof reducerMap>>
-   * } Payloads
-   */
-
-  return /** @type {ReduxCompatibleReducer<S, Payloads>} */(ret);
-};
 
 export default handleActions(
   {
